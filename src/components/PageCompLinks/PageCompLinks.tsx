da141da238e434d9bc6449ca598e9b1b9f91339f
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-04-13 16:57:44 -0700

    Remove reference to discontinued VertMenuListTitle component

--- src/components/PageCompLinks/PageCompLinks.tsx
+++ src/components/PageCompLinks/PageCompLinks.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
 
-import { VertMenuListTitle } from "../VerticalMenu/VertMenuListTitle/VertMenuListTitle";
 import "./PageCompLinks.css";
 
 interface Props {
@@ -12,7 +11,6 @@ interface Props {
 export const PageCompLinks: FC<Props> = ({ onVertMenu, otherLinks }) => {
   return (
     <React.Fragment>
-      {onVertMenu && <VertMenuListTitle title="Other Links" />}
       {otherLinks?.map((otherLink) => (
         <Link className="dropdown-item-link" to={otherLink.link}>
           <li className={`${onVertMenu && "page-comp-link-item"}`}>
